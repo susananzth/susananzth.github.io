97a7de48f40a708db940424e477a5b5e8c7118bf
susananzth <susananzth.dev@gmail.com>
2023-04-26 16:14:09 -0500

    Correccion y mejora en skills

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="container">
-            <footer className="d-md-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <footer className="d-md-flex flex-wrap justify-content-between align-items-center py-3 border-top">
                 <div className="col d-flex align-items-center justify-c-sm-center">
                     <a href="/" className="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
                         <img src={myLogo} className="logo" alt="Logo de susananzth" />
